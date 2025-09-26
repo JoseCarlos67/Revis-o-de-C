d174c5faa4fed7c7b8e619259591517be44f6860
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2025-09-26 16:03:50 -0300

    Exercicio 09 concluido

--- Matrizes/09.exercicio/matriz.c
+++ Matrizes/09.exercicio/matriz.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int preencheMatriz(int matriz[4][4])
+#define tam 4
+
+int preencheMatriz(int matriz[tam][tam])
 {
   FILE *arquivo = fopen("entrada.txt", "r");
   if (arquivo == NULL)
@@ -12,8 +14,8 @@ int preencheMatriz(int matriz[4][4])
     return 1;
   }
 
-  for (int i = 0; i < 4; i++)
-    for (int j = 0; j < 4; j++)
+  for (int i = 0; i < tam; i++)
+    for (int j = 0; j < tam; j++)
     {
       if (fscanf(arquivo, "%d", &matriz[i][j]) != 1)
       {
@@ -25,16 +27,49 @@ int preencheMatriz(int matriz[4][4])
   fclose(arquivo);
 }
 
-void verificaQuadradoMagico(int matriz[4][4])
+int somaDiagonalPrincipal(int matriz[tam][tam])
 {
+  int soma = 0;
+  for (int i = 0; i < tam; i++)
+    soma += matriz[i][i];
 
+  return soma;
 }
 
-void imprimeMatriz(int matriz[4][4])
+int somaDiagonalSecundaria(int matriz[tam][tam])
 {
-  for (int i = 0; i < 4; i++)
+  int soma = 0;
+  for (int i = 0; i < tam; i++)
+    soma += matriz[i][tam - 1 - i];
+
+  return soma;
+}
+
+void somaColunas(int matriz[tam][tam], int somaCol[tam])
+{
+  for (int j = 0; j < tam; j++)
+    somaCol[j] = 0;
+
+  for (int i = 0; i < tam; i++)
+    for (int j = 0; j < tam; j++)
+      somaCol[j] += matriz[i][j];
+}
+
+void somaLinhas(int matriz[tam][tam], int somaLin[tam])
+{
+  for (int i = 0; i < tam; i++)
+    somaLin[i] = 0; // zera o vetor
+
+  for (int i = 0; i < tam; i++)
+    for (int j = 0; j < tam; j++)
+      somaLin[i] += matriz[i][j]; // soma por linha
+}
+
+void imprimeMatriz(int matriz[tam][tam])
+{
+  for (int i = 0; i < tam; i++)
   {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < tam; j++)
     {
       printf("%2d ", matriz[i][j]);
     }
@@ -42,12 +77,62 @@ void imprimeMatriz(int matriz[4][4])
   }
 }
 
+void verificaQuadradoMagico(int matriz[tam][tam])
+{
+  int falhou = 0;
+  int somaDP = somaDiagonalPrincipal(matriz);
+  int somaDS = somaDiagonalSecundaria(matriz);
+  if (somaDS != somaDP)
+  {
+    printf("\nDiagonais diferentes!");
+    falhou = 1;
+  }
+  else
+    printf("\nDiagonais iguais!");
+
+  int somaCol[tam];
+  somaColunas(matriz, somaCol);
+  int verificaSomaColELin = 0;
+  for (int i = 1; i < tam; i++)
+    if (somaCol[i - 1] == somaCol[i])
+      verificaSomaColELin++;
+  if (verificaSomaColELin == tam - 1)
+    printf("\nSoma das colunas iguais!");
+  else
+  {
+    printf("\nSoma das colunas diferentes!");
+    falhou = 1;
+  }
+
+  int somaLin[tam];
+  verificaSomaColELin = 0;
+  somaLinhas(matriz, somaLin);
+  int verificaSomaLin = 0;
+  for (int i = 1; i < tam; i++)
+    if (somaLin[i - 1] == somaLin[i])
+      verificaSomaColELin++;
+  if (verificaSomaColELin == tam - 1)
+    printf("\nSoma das linhas iguais!");
+  else
+  {
+    printf("\nSoma das linhas diferentes!");
+    falhou = 1;
+  }
+
+  if (falhou == 0)
+    printf("\nEH QUADRADO MAGICO!");
+  else
+    printf("\nNAO EH QUADRADO MÁGICO!");
+
+  printf("\n");
+}
+
 int main(int argc, char const *argv[])
 {
-  int matriz[4][4];
+  int matriz[tam][tam];
   preencheMatriz(matriz);
-  printf("Matriz original:\n");
+  printf("Matriz:\n");
   imprimeMatriz(matriz);
-  printf("\n");
+  verificaQuadradoMagico(matriz);
   return 0;
 }
